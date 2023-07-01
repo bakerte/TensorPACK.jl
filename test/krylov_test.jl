afc35c21a52ad43a07f4c4055cd53a41b7d0d7dd
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2023-07-01 00:58:46 -0700

    Add files via upload

--- test/krylov_test.jl
+++ test/krylov_test.jl
@@ -13,7 +13,7 @@ A += A'
 psi = rand(m)
 
 using BenchmarkTools
-#@btime krylov(psi,A)
+#@time krylov(psi,A)
 
 psi,A = tens(psi),tens(A)
 
@@ -22,7 +22,7 @@ psisave,D = lanczos(psi,A,maxiter=m,reorth=true)
 P = [dot(psisave[i],psisave[j]) for i = 1:length(psisave),j=1:length(psisave)]
 testval = isapprox(sum(w->P[w,w],1:size(P,1)),size(P,1)) && abs(sum(P) - size(P,1)) < 1E-3
 #=
-@btime krylov(psi,A)
+@time krylov(psi,A)
 
 D,U = libeigen(alpha,beta)
 =#
--- test/svdtest.jl
+++ test/svdtest.jl
@@ -40,12 +40,12 @@ end
 rA = reshape!(A,prod(size(A)[Linds]),prod(size(A)[Rinds]))
 
 @time U,D,V = svd(rA)
-@btime svd(rA)
+@time svd(rA)
 println()
 println("...now truncating svd")
 @time U,D,V = svd(rA,m=40)
-@btime U,D,V = svd(rA,m=40)
+@time U,D,V = svd(rA,m=40)
 
 #U,D,V = svd(A,[Linds,Rinds])
-#@btime svd(A,[Linds,Rinds])
+#@time svd(A,[Linds,Rinds])
 
