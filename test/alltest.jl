92e7b8faab3fa1ad91fbba23e775b373406849ab
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2023-10-27 15:53:25 -0700

    Update alltest.jl

--- test/alltest.jl
+++ test/alltest.jl
@@ -54,7 +54,7 @@ function testfct(test::Bool,message::String)
   
   See also: [`libdir`](@ref)
   """
-  function libtest(;tests::Array{String,1}=tests,dir::String=libdir,path::String=dir*"/test/")
+  function libtest(;tests::Array{String,1}=tests,dir::String=libdir,path::String=dir*"/../test/")
   
     fulltestrecord = Array{Bool,1}(undef,length(tests))
   
