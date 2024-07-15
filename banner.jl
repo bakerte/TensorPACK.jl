29ec70196a6d21b96f2715449394f03c361fefbb
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-07-15 16:18:11 +0200

    Add files via upload

---
+++ banner.jl
@@ -0,0 +1,10 @@
+#########################################################################
+#
+#           Tensor Linear Algebra Package (TENPACK)
+#                          v1.0
+#
+#########################################################################
+# Made by Thomas E. Baker and « les qubits volants » (2024)
+# See accompanying license with this program
+# This code is native to the julia programming language (v1.10.4+)
+#
