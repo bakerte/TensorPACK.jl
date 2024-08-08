35dfa45921aaa1893c0a8bf52491e9bb1ad06ceb
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-08-07 23:46:29 -0700

    Update TensorPACK.jl

--- src/TensorPACK.jl
+++ src/TensorPACK.jl
@@ -40,109 +40,58 @@ and define functions as `LinearAlgebra.svd` to use functions from that package.
 
 """
 module TensorPACK
-import LinearAlgebra
-import Printf
-import Distributed
-
-println(" _____ _____ _   _ ______  ___  _____  _   __ ")
-println("|_   _|  ___| \\ | || ___ \\/ _ \\/  __ \\| | / / ")
-println("  | | | |__ |  \\| || |_/ / /_\\ \\ /  \\/| |/ /  ")
-println("  | | |  __|| . ` ||  __/|  _  | |    |    \\  ")
-println("  | | | |___| |\\  || |   | | | | \\__/\\| |\\  \\ ")
-println("  \\_/ \\____/\\_| \\_/\\_|   \\_| |_/\\____/\\_| \\_/ ")
-
-println("version 1.0")
-println("(made for julia v1.10.4+, see included license)")
-println()
-println("Code: https://github.com/bakerte/TENPACK.jl")
-println()
-println("Please cite: T.E. Baker, \"forthcoming\"")
-println("...and any other algorithms used (noted in documentation)")
-println()
-println("Funding for this program is graciously provided by:")
-println("   + Institut quantique (Université de Sherbrooke)")
-println("   + Département de physique, Université de Sherbrooke")
-println("   + Canada First Research Excellence Fund (CFREF)")
-println("   + Institut Transdisciplinaire d'Information Quantique (INTRIQ)")
-println("   + US-UK Fulbright Commission (Bureau of Education and Cultural Affairs from the United States Department of State)")
-println("   + Department of Physics, University of York")
-println("   + Canada Research Chair in Quantum Computing for Modeling of Molecules and Materials")
-println("   + Department of Physics & Astronomy, University of Victoria")
-println("   + Department of Chemistry, University of Victoria")
-println("   + Faculty of Science, University of Victoria")
-println("   + National Science and Engineering Research Council (NSERC)")
 
 const TENPACK = TensorPACK
 export TENPACK
 
+   const libdir = @__DIR__
+   const libpath = libdir*"/lib/"
 
-#println("Running the julia kernel with --check-bounds=no can decrease runtimes by 20%")
-juliathreads = Threads.nthreads()
-println("julia threads: ",juliathreads,"    (modify with 'export JULIA_NUM_THREADS=' or `julia -t #`)")
-println("julia processes: ",Distributed.nprocs(),"    (modify with 'Distributed' package commands, `addprocs()` or `julia -p #`)")
-LinearAlgebra.BLAS.set_num_threads(juliathreads)
-#println("BLAS threads (set in DMRjulia.jl): ",juliathreads)
-#println("BLAS threads: ",ccall((:openblas_get_num_threads64_, Base.libblas_name), Cint, ()))
-println()
-println("# Warning:")
-println("We recommend not defining `using LinearAlgebra` to avoid conflicts.  Instead, define")
-println("```\n
-import LinearAlgebra\n
-```\n")
-println("and define functions as `LinearAlgebra.svd` to use functions from that package.")
-println()
+   files = ["imports.jl","banner.jl","types.jl","tensordef.jl","largevector.jl","libalg.jl","isapprox.jl"]
+   for w = 1:length(files)
+      include(libpath*files[w])
+   end
 
+   subdir = "tensor/"
+   files = ["tens.jl","diagonal.jl","Array.jl","undefMat.jl","dtens.jl","directedtens.jl","nametens.jl","network.jl"]
+   for w = 1:length(files)
+      include(libpath*subdir*files[w])
+   end
 
-const libdir = @__DIR__
-
-libpath = libdir*"/../lib/"
-
-#Linear algebra routines
-
-files = ["types.jl","tensordef.jl","largevector.jl","libalg.jl","isapprox.jl"]
-for w = 1:length(files)
-   include(libpath*files[w])
-end
-
-subdir = "tensor/"
-files = ["tens.jl","diagonal.jl","Array.jl","undefMat.jl","dtens.jl","directedtens.jl","network.jl"]
-for w = 1:length(files)
-   include(libpath*subdir*files[w])
-end
-
-subdir = "methods/"
-files = ["rand.jl","zeros.jl","ones.jl","eye.jl","eltype.jl","root.jl","size.jl","length.jl","conj.jl","copy.jl","setindex.jl","getindex.jl","reshape.jl","exp.jl","invmat.jl","sqrt.jl","positionindex.jl","tensor_ranges.jl","minmax.jl","transpose.jl","adjoint.jl","directsum.jl","checktype.jl","addmultsubdiv.jl","sum.jl","norm.jl","convIn.jl","permute.jl","tensorcombination.jl","display.jl","print.jl","nametens.jl","ndims.jl","dual.jl","rename.jl","swapname.jl","joinTens.jl","dualnum.jl","swapgate.jl","det.jl"]
-for w = 1:length(files)
-   include(libpath*subdir*files[w])
-end
+   subdir = "methods/"
+   files = ["plus.jl","abs.jl","add.jl","caret.jl","div.jl","log.jl","minus.jl","mult.jl","other.jl","slash.jl","star.jl","sub.jl","rand.jl","zeros.jl","ones.jl","eye.jl","eltype.jl","size.jl","length.jl","conj.jl","copy.jl","setindex.jl","getindex.jl","reshape.jl","exp.jl","invmat.jl","sqrt.jl","positionindex.jl","tensor_ranges.jl","minmax.jl","transpose.jl","adjoint.jl","directsum.jl","checktype.jl","sum.jl","norm.jl","convIn.jl","permute.jl","tensorcombination.jl","display.jl","print.jl","ndims.jl","dual.jl","rename.jl","swapname.jl","joinTens.jl","dualnum.jl","root.jl","swapgate.jl","det.jl","convertTens.jl"]
+   for w = 1:length(files)
+      include(libpath*subdir*files[w])
+   end
 
 
 
 
-subdir = "qnumber/"
-include(libpath*subdir*"qnum.jl")
+   subdir = "qnumber/"
+   include(libpath*subdir*"qnum.jl")
 
-files = [#="orderblocks.jl",=#"QnumList.jl","matchblocks.jl","changeblock.jl","findextrablocks.jl","Qtens.jl","checkflux.jl"]
-for w = 1:length(files)
-   include(libpath*subdir*files[w])
-end
-
-
-subdir = "contract/"
-files = ["dot.jl","dmul.jl","diagcontract.jl","maincontractor.jl","contract.jl","ccontract.jl","contractc.jl","ccontractc.jl","star.jl","autocontract.jl","trace.jl","checkcontract.jl"]
-for w = 1:length(files)
-   include(libpath*subdir*files[w])
-end
+   files = [#="orderblocks.jl",=#"QnumList.jl","matchblocks.jl","changeblock.jl","findextrablocks.jl","Qtens.jl","checkflux.jl"]
+   for w = 1:length(files)
+      include(libpath*subdir*files[w])
+   end
 
 
-subdir = "decompose/"
-files = ["decompositions.jl","truncate.jl","svd.jl","svdvals.jl","eigen.jl","eigvals.jl","lq.jl","qr.jl","polar.jl","Krylov.jl","nullspace.jl"]
-for w = 1:length(files)
-   include(libpath*subdir*files[w])
-end
+   subdir = "contract/"
+   files = ["dot.jl","dmul.jl","diagcontract.jl","maincontractor.jl","contract.jl","ccontract.jl","contractc.jl","ccontractc.jl","star.jl","autocontract.jl","trace.jl","strassOPen.jl","strassen.jl","checkcontract.jl"]
+   for w = 1:length(files)
+      include(libpath*subdir*files[w])
+   end
+
+
+   subdir = "decompose/"
+   files = ["decompositions.jl","truncate.jl","svd.jl","svdvals.jl","eigen.jl","eigvals.jl","lq.jl","qr.jl","polar.jl","Krylov.jl","nullspace.jl"]
+   for w = 1:length(files)
+      include(libpath*subdir*files[w])
+   end
+
+
 
 const testpath = libdir*"/../test/"
-
 include(testpath*"alltest.jl")
 
 end
