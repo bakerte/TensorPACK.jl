6d8ea3d26cecbfff982be9bcdc5a2c28da9f3d9f
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-08-14 12:14:24 -0700

    Add files via upload

--- lib/methods/caret.jl
+++ lib/methods/caret.jl
@@ -1,10 +1,29 @@
+###############################################################################
+#
+#  Density Matrix Renormalization Group (and other methods) in julia (DMRjulia)
+#                               v1.0
+#
+###############################################################################
+# Made by Thomas E. Baker and « les qubits volants » (2024)
+# See accompanying license with this program
+# This code is native to the julia programming language (v1.10.0+)
+#
 
+"""
+    C = ^(A,B)
 
+Computes `A^B` for a diagonal matrix `A` by a power `B`
+"""
 function ^(A::Diagonal{W},P::B) where {W <: Number, B <: Number}
   C = A.T .^ P
   return Diagonal(C)
 end
 
+"""
+    C = ^(A,B)
+
+Computes `A^B` for a `denstens` matrix `A` by a power `B`
+"""
 function ^(A::tens{W},P::B) where {W <: Number, B <: Number}
   if B <: Integer
     C = prod(w->A,1:P)
@@ -24,4 +43,15 @@ function ^(x::dualnum, y::Number)
   r = x.val ^ y
   g = y*x.val^(y-1) * x.grad
   return dualnum(r, g)
+end
+
+
+"""
+    C = ^(A,B)
+
+Computes `A^B` for a `denstens` matrix `A` by a power `B`
+"""
+function ^(A::nametens,P::B) where {W <: Number, B <: Number}
+  @assert(ndims(A)==2)
+  return nametens(A.N^P,A.names)
 end
