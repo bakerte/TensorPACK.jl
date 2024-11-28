e3d67d86867e3b99fc71ae5b2b8d923c481b273e
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-11-28 11:21:22 -0800

    Update alltest.jl

--- test/alltest.jl
+++ test/alltest.jl
@@ -218,7 +218,7 @@ const tests = [
 ]
 
 """
-  testlib([,tests=,path=libdir*"/test/"])
+  testlib([,tests=,path=libdir*"..//test/"])
 
 Tests all functions in the files enumerated in `tests`. Default is to check all test functionality in the library. Used in nightly builds. See format in `/tests/` folder
 
